ed0af024f86a36f81f1f3b166d3f79decf7023a0
Christian Kiely <obimaceyoda@gmail.com>
2015-04-23 14:41:35 +1000

    Basis for locking down client side

--- loveletter.js
+++ loveletter.js
@@ -1,18 +1,20 @@
 Games = new Meteor.Collection('games');
 
 if (Meteor.isServer) {
-	Meteor.publish('games', function () {
-        return Games.find();
+	Meteor.publish('games', function (id) {
+
+		return Games.find({}, {fields: {currentTurn:1, owner:1, inProgress:1,lobby:1,log:1,started:1,faceup:1, protected:1, scores:1, discards:1, lastDiscarded:1, eliminated:1, betweenRounds:1, lastRoundWinner:1, lastTurn:1, bigWinner:1, players:1, deck:1 }})
+
     });
 
     Meteor.publish('users', function () {
-        return Meteor.users.find();
+        return Meteor.users.find({}, {fields: {profile: 1, username: 1}});
     });
 }
 
 if (Meteor.isClient) {
 
-	Meteor.subscribe('games');
+	Meteor.subscribe('games', Meteor.userId());
 	Meteor.subscribe('users');
 
 	Template.registerHelper("username", function(id) {
